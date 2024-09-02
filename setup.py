fed32114874eda660fd918a864db5c12f92eebda
Endybits <endyb.dev@gmail.com>
2024-09-02 14:07:37 +1000

    v0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dialogic-ai',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(include=['dialogic_ai', 'dialogic_ai.*']),
     install_requires=[
     ],
